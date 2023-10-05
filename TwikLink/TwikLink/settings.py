ba03502b335c8886e3bfbecc122da6d6ff40070f
Mahdi <mahdiesscv69@gmail.com>
2023-10-05 21:53:59 +0100

    gg

--- TwikLink/TwikLink/settings.py
+++ TwikLink/TwikLink/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS  = [os.path.join(BASE_DIR,'TwikLink/static'),]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'mediacdn')
 
 
 # STATIC_URL = '/static/'
