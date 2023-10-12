1a6cb3d3c4940e20670bb29113f2fa01ec9bf8ca
Mahdi <mahdiesscv69@gmail.com>
2023-10-12 14:48:16 +0100

    gg

--- TwikLink/TwikLink/urls.py
+++ TwikLink/TwikLink/urls.py
@@ -7,8 +7,6 @@ from django.views.static import serve
 
 
 urlpatterns = [
-    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('mahdiesscv69/admin/', admin.site.urls),
     path('', include("Profile.urls")),
     path('accounts/', include('allauth.urls')),
@@ -18,5 +16,4 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
 
 handler404 = 'Profile.views.error_404_view'
-#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
